32ef26d0672bd4175bdccd378b7fade75996e3d2
apellizz <alberto.pellizzon@hotmail.it>
2014-11-23 03:34:23 +0100

    added images

--- app/scripts/controllers/metric.js
+++ app/scripts/controllers/metric.js
@@ -13,11 +13,12 @@ angular.module('hackfashionApp')
     
 
   	$scope.selected = [
-    	{id: 'p2', price: 7, file: 'texture1.jpg', title: 'Energy', author: 'Van Gogh', src: "images/woman/textures/texture1.jpg"},
-    	{id: 'p3', price: 7, file: 'texture2.jpg', title: 'Creativity', author: 'Pablo Picasso', src: "images/woman/textures/texture2.jpg"},
-			{id: 'p2', price: 7, file: 'texture3.jpg', title: 'Fantasy', author: 'Dali', src: "images/woman/textures/texture3.jpg"},
-    	{id: 'p3', price: 7, file: 'texture4.jpg', title: 'Caos', author: 'Da Vinci', src: "images/woman/textures/texture4.jpg"}
-		];
+    	{id: 'p2', price: 30, file: 'texture1.jpg', title: 'Energy', author: 'Van Gogh', src: "images/woman/textures/texture1.jpg"},
+      {id: 'p3', price: 50, file: 'texture2.jpg', title: 'Creativity', author: 'Pablo Picasso', src: "images/woman/textures/texture2.jpg"},
+      {id: 'p2', price: 70, file: 'texture3.jpg', title: 'Fantasy', author: 'Dali', src: "images/woman/textures/texture3.jpg"},
+      {id: 'p3', price: 80, file: 'texture4.jpg', title: 'Caos', author: 'Da Vinci', src: "images/woman/textures/texture4.jpg"},
+      {id: 'p3', price: 100, file: 'texture5.jpg', title: 'Liquid', author: 'Caravaggio', src: "images/woman/textures/texture5.jpg"}
+    ];
 
 		$scope.set = function(card,id){
 			$scope.texture = card;
--- app/scripts/controllers/tailoring.js
+++ app/scripts/controllers/tailoring.js
@@ -11,10 +11,11 @@ angular.module('hackfashionApp')
   .controller('TailoringCtrl', function ($scope,$location,$timeout) {
     var marker, marker2, map;
   	$scope.selected = [
-    	{id: 'p2', price: 7, file: 'texture1.jpg', title: 'Energy', author: 'Van Gogh', src: "images/woman/textures/texture1.jpg"},
-      {id: 'p3', price: 7, file: 'texture2.jpg', title: 'Creativity', author: 'Pablo Picasso', src: "images/woman/textures/texture2.jpg"},
-      {id: 'p2', price: 7, file: 'texture3.jpg', title: 'Fantasy', author: 'Dali', src: "images/woman/textures/texture3.jpg"},
-      {id: 'p3', price: 7, file: 'texture4.jpg', title: 'Caos', author: 'Da Vinci', src: "images/woman/textures/texture4.jpg"}
+    	{id: 'p2', price: 30, file: 'texture1.jpg', title: 'Energy', author: 'Van Gogh', src: "images/woman/textures/texture1.jpg"},
+      {id: 'p3', price: 50, file: 'texture2.jpg', title: 'Creativity', author: 'Pablo Picasso', src: "images/woman/textures/texture2.jpg"},
+      {id: 'p2', price: 70, file: 'texture3.jpg', title: 'Fantasy', author: 'Dali', src: "images/woman/textures/texture3.jpg"},
+      {id: 'p3', price: 80, file: 'texture4.jpg', title: 'Caos', author: 'Da Vinci', src: "images/woman/textures/texture4.jpg"},
+      {id: 'p3', price: 100, file: 'texture5.jpg', title: 'Liquid', author: 'Caravaggio', src: "images/woman/textures/texture5.jpg"}
     ];
 
     $scope.$on('mapInitialized', function(evt, evtMap) {
