883653843032e638f7579abb0a19ea0be61c718b
apellizz <alberto.pellizzon@hotmail.it>
2014-11-24 21:08:05 +0100

    removed imagein

--- Gruntfile.js
+++ Gruntfile.js
@@ -339,9 +339,7 @@ module.exports = function (grunt) {
         'copy:styles'
       ],
       dist: [
-        'copy:styles',
-        'imagemin',
-        'svgmin'
+        'copy:styles'
       ]
     },
 
